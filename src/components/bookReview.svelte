65e1e05719e287068d83785c2e738f530a83c92d
Levi Villarreal <villarreallevi@gmail.com>
2025-10-11 00:00:20 -0400

    Componitze the review

---
+++ src/components/bookReview.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { Book } from './bookTypes';
+
+	export let book: Book;
+
+	function getReviewDate() {
+		// Sometimes the user doesn't specify a read date
+		// fall back to the added date which should always exist
+		const dateStr = book['Date Read'] || book['Date Added'];
+		if (!dateStr) {
+			return 'N/A';
+		}
+		const date = new Date(dateStr);
+		return date.toLocaleDateString(undefined, {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		});
+	}
+
+	function getReview() {
+		const reviewText = book['My Review'] || '';
+		return reviewText.replace(/\n/g, '<br/>');
+	}
+</script>
+
+<hr />
+<div class="review">
+	<h2>My review of {book.Title}</h2>
+	<h4>Reviewed on {getReviewDate()}</h4>
+	<p>My rating: {book['My Rating']} / 5</p>
+	<p>Average rating: {book['Average Rating']} / 5</p>
+	<p>{@html getReview()}</p>
+</div>
+
+<style>
+	.review {
+		max-width: 800px;
+		margin: 0 auto;
+		padding: 0 16px;
+	}
+
+	.review h2 {
+		font-family: 'Yeseva One', serif;
+	}
+</style>
--- src/components/bookshelf.svelte
+++ src/components/bookshelf.svelte
@@ -2,6 +2,7 @@
 	import Papa from 'papaparse';
 	import type { Book } from './bookTypes.js';
 	import BookItem from './bookItem.svelte';
+	import BookReview from './bookReview.svelte';
 
 	export let bookData: string;
 
@@ -25,9 +26,7 @@
 		if (!parsedData.data) return [];
 
 		let filteredBooks = parsedData.data
-			// Filter by only read books
 			.filter((book: Book) => book['Read Count'] > 0)
-			// Filter by only books with reviews
 			.filter((book: Book) => book['My Review']);
 
 		// Sort by date read, most recent first
@@ -40,19 +39,6 @@
 		return filteredBooks;
 	})();
 
-	// Filter by only read books
-	books = books.filter((book) => book['Read Count'] > 0);
-
-	// Filter by only books with reviews
-	books = books.filter((book) => book['My Review']);
-
-	// Sort by date read, most recent first
-	books.sort((a, b) => {
-		const dateA = new Date(a['Date Read'] || a['Date Added'] || '');
-		const dateB = new Date(b['Date Read'] || b['Date Added'] || '');
-		return dateB.getTime() - dateA.getTime();
-	});
-
 	if (errors.length) {
 		console.log('errors', errors);
 	}
@@ -62,27 +48,6 @@
 	const bookWidth = width * 5;
 
 	$: book = currentBookIndex !== -1 ? books[currentBookIndex] : null;
-	$: review = (() => {
-		if (!book) return '';
-
-		const reviewText = book['My Review'] || '';
-		return reviewText.replace(/\n/g, '<br/>');
-	})();
-	$: reviewDate = (() => {
-		if (!book) return '';
-
-		const dateStr = book['Date Read'] || book['Date Added'];
-		if (!dateStr) {
-			return 'N/A';
-		}
-		const date = new Date(dateStr);
-		return date.toLocaleDateString(undefined, {
-			year: 'numeric',
-			month: 'long',
-			day: 'numeric'
-		});
-	})();
-	$: bookTitle = book ? book.Title : '';
 
 	$: bookClicked = (index: number) => {
 		currentBookIndex = index === currentBookIndex ? -1 : index;
@@ -111,14 +76,7 @@
 </div>
 
 {#if currentBookIndex !== -1 && book}
-	<hr />
-	<div class="review">
-		<h2>My review of {bookTitle}</h2>
-		<h4>Reviewed on {reviewDate}</h4>
-		<p>My rating: {book['My Rating']} / 5</p>
-		<p>Average rating: {book['Average Rating']} / 5</p>
-		<p>{@html review}</p>
-	</div>
+	<BookReview {book} />
 {/if}
 
 <style>
@@ -144,15 +102,5 @@
 		transition: all 500ms ease;
 		will-change: auto;
 		width: var(--width);
-	}
-
-	.review {
-		max-width: 800px;
-		margin: 0 auto;
-		padding: 0 16px;
-	}
-
-	.review h2 {
-		font-family: 'Yeseva One', serif;
 	}
 </style>
